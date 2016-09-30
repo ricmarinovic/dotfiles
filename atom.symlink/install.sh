569108a3815fab68d743a1b4c8f156ccfdef77ea
Ricardo Marinovic <marinovic.ricardo@gmail.com>
2016-09-29 23:33:51 -0300

    Add apm installs

--- atom.symlink/install.sh
+++ atom.symlink/install.sh
@@ -3,7 +3,20 @@
 # Installs academie theme if apm is installed
 if test ! $(which apm)
 then
+  echo "Installing atom packages ..."
   apm install academie-syntax
+  apm install language-babel
+  apm install language-elixir
+  apm install language-erlang
+  apm install linter
+  apm install linter-elixirc
+  apm install linter-js-standard
+  apm install linter-rubocop
+  apm install minimap
+  apm install minimap-bookmarks
+  apm install minimap-cursorline
+  apm install minimap-git-diff
+  apm install pigments
 else
   echo "Academie theme not installed!"
   echo "Open atom and install shell commands!"
